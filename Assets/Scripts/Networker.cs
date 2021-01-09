d43a928110b78ba9f063bb01e1094dc082ff6416
Avea <aveakrause@gmail.com>
2021-01-09 14:35:55 -0800

    Updated server ip

--- Assets/Scripts/Networker.cs
+++ Assets/Scripts/Networker.cs
@@ -18,24 +18,27 @@ public class Networker : MonoBehaviour
         TryConnect(out client);
 
         Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-        NetworkStream stream = client.GetStream();
-        
-        Debug.Log($"Sending message: {message} to server.");
-        stream.Write(data, 0, data.Length);
-        Debug.Log("Message sent.");
-        
-        data = new Byte[256];
-        string responseData = string.Empty;
+        if(client != null)
+        {
+            NetworkStream stream = client.GetStream();
+            
+            Debug.Log($"Sending message: {message} to server.");
+            stream.Write(data, 0, data.Length);
+            Debug.Log("Message sent.");
+            
+            data = new Byte[256];
+            string responseData = string.Empty;
 
-        Debug.Log("Reading response from server...");
-        int bytes = stream.Read(data, 0, data.Length);
-        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-        Debug.Log($"Response from server: {responseData}");
+            Debug.Log("Reading response from server...");
+            int bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Debug.Log($"Response from server: {responseData}");
 
-        Debug.Log("Disconnecting from server.");
-        stream.Close();
-        client.Close();
-        Debug.Log("Sucessfully disconnected from server.");
+            Debug.Log("Disconnecting from server.");
+            stream.Close();
+            client.Close();
+            Debug.Log("Sucessfully disconnected from server.");
+        }
     }
 
     bool TryConnect(out TcpClient c)
